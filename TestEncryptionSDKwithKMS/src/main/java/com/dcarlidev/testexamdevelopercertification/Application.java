e56c3908d7efd983371700cf4c9c9bac343aa7a5
cglez1987 <cglez1987@gmailcom>
2020-05-01 22:53:36 -0400

    Changing some examples in TestEncryptionSDKWithKMS project

--- TestEncryptionSDKwithKMS/src/main/java/com/dcarlidev/testexamdevelopercertification/Application.java
+++ TestEncryptionSDKwithKMS/src/main/java/com/dcarlidev/testexamdevelopercertification/Application.java
@@ -9,14 +9,22 @@ import com.amazonaws.encryptionsdk.AwsCrypto;
 import com.amazonaws.encryptionsdk.CryptoResult;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKey;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
-//import software.amazon.awssdk.core.SdkBytes;
-//import software.amazon.awssdk.services.kms.KmsClient;
-//import software.amazon.awssdk.services.kms.model.DecryptRequest;
-//import software.amazon.awssdk.services.kms.model.DecryptResponse;
-//import software.amazon.awssdk.services.kms.model.EncryptRequest;
-//import software.amazon.awssdk.services.kms.model.EncryptResponse;
-//import software.amazon.awssdk.services.kms.model.GenerateDataKeyRequest;
-//import software.amazon.awssdk.services.kms.model.GenerateDataKeyResponse;
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.kms.model.DecryptRequest;
+import software.amazon.awssdk.services.kms.model.DecryptResponse;
+import software.amazon.awssdk.services.kms.model.EncryptRequest;
+import software.amazon.awssdk.services.kms.model.EncryptResponse;
+import software.amazon.awssdk.services.kms.model.GenerateDataKeyRequest;
+import software.amazon.awssdk.services.kms.model.GenerateDataKeyResponse;
 
 /**
  *
@@ -28,36 +36,45 @@ public class Application {
 
     public static void main(String... args) {
         String plaintext = "testing the encryption";
+//        byte[] var = null;
+//        try {
+//            var = Files.readAllBytes(Paths.get(""));
+//        } catch (IOException ex) {
+//            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+//        }
 
         /*
         Encrypt and decrypt using AWS KMS service directly.
          */
-//        KmsClient client = KmsClient.create();
-//        EncryptResponse response = client.encrypt(EncryptRequest.builder().
-//                keyId(KEY_ARN).
-//                plaintext(SdkBytes.fromUtf8String(plaintext)).build());
-//        SdkBytes text_encrypted = response.ciphertextBlob();
-//        System.out.println("Text Encrypted: " + Base64.getEncoder().encodeToString(text_encrypted.asByteArray()));
-//
-//        DecryptResponse decrypResponse = client.decrypt(DecryptRequest.builder().ciphertextBlob(text_encrypted).build());
-//        SdkBytes text_decrypted = decrypResponse.plaintext();
-//        System.out.println("TEXT DECRYPTED: " + new String(text_decrypted.asByteArray()));
+        KmsClient client = KmsClient.create();
+        EncryptResponse response = client.encrypt(EncryptRequest.builder().
+                keyId(KEY_ARN).
+                plaintext(SdkBytes.fromUtf8String(plaintext)).build());
+        SdkBytes text_encrypted = response.ciphertextBlob();
+        System.out.println("Text Encrypted: " + Base64.getEncoder().encodeToString(text_encrypted.asByteArray()));
+
+        DecryptResponse decrypResponse = client.decrypt(DecryptRequest.builder().ciphertextBlob(text_encrypted).build());
+        SdkBytes text_decrypted = decrypResponse.plaintext();
+        System.out.println("TEXT DECRYPTED: " + new String(text_decrypted.asByteArray()));
+
+        GenerateDataKeyResponse responseDataKey = client.generateDataKey(GenerateDataKeyRequest.builder().keyId(KEY_ARN).numberOfBytes(2).build());
+        System.out.println("GenerateDataKey CipherText: " + new String(responseDataKey.ciphertextBlob().asByteArray()));
+        System.out.println("GenerateDataKey PlainText: " + new String(responseDataKey.plaintext().asByteArray()));
 
         /*
         Encrypt and decrypt using AWS Encryption SDK
          */
-        AwsCrypto crypto = new AwsCrypto();
-        KmsMasterKeyProvider provider = KmsMasterKeyProvider.builder().withDefaultRegion("us-east-1").
-                withKeysForEncryption(KEY_ARN).build();
-        CryptoResult<byte[], KmsMasterKey> resultEncryp = crypto.encryptData(provider, plaintext.getBytes());
-        final String ciphertext = new String(resultEncryp.getResult());
-
-        System.out.println("Using Crypto SDK - Ciphertext: " + ciphertext);
-
-        final CryptoResult<byte[], KmsMasterKey> result = crypto.decryptData(provider, resultEncryp.getResult());
-
-        System.out.println("Using Crypto SDK - Ciphertext: " + new String(result.getResult()));
-
+//        AwsCrypto crypto = new AwsCrypto();
+//        KmsMasterKeyProvider provider = KmsMasterKeyProvider.builder().withDefaultRegion("us-east-1").
+//                withKeysForEncryption(KEY_ARN).build();
+//        CryptoResult<byte[], KmsMasterKey> resultEncryp = crypto.encryptData(provider, plaintext.getBytes());
+//        final String ciphertext = new String(resultEncryp.getResult());
+//
+//        System.out.println("Using Crypto SDK - Ciphertext: " + ciphertext);
+//
+//        final CryptoResult<byte[], KmsMasterKey> result = crypto.decryptData(provider, resultEncryp.getResult());
+//
+//        System.out.println("Using Crypto SDK - Ciphertext: " + new String(result.getResult()));
     }
 
 }
